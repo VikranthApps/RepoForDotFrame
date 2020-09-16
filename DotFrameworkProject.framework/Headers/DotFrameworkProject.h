a944001e76a294580000859d6420b0831a1f7323
Vikranth Apps <imakingapps@gmail.com>
2020-09-16 13:52:27 +0530

    dot framework project

---
+++ DotFrameworkProject.framework/Headers/DotFrameworkProject.h
@@ -0,0 +1,19 @@
+//
+//  DotFrameworkProject.h
+//  DotFrameworkProject
+//
+//  Created by Vikranth Naidu on 16/09/20.
+//  Copyright © 2020 Vikranth Naidu. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for DotFrameworkProject.
+FOUNDATION_EXPORT double DotFrameworkProjectVersionNumber;
+
+//! Project version string for DotFrameworkProject.
+FOUNDATION_EXPORT const unsigned char DotFrameworkProjectVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DotFrameworkProject/PublicHeader.h>
+
+
